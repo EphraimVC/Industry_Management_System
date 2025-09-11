eff586b492136228f744bd4a744b6b7e13904ef1
Ephraim Valladares <effelion@gmail.com>
2025-09-11 14:20:30 +0200

    added gql mutations

--- GQL/schema/typeDefs.js
+++ GQL/schema/typeDefs.js
@@ -32,11 +32,55 @@ export const typeDefs = /* GraphQL */ `
         totalStockValueByManufacturer: [ManufacturerStockValue!]!
         LowStockProducts: [Product!]!
         manufacturers: [Manufacturer!]!
-        criticalStockProducts (inStock:amountInStock, ):
+        criticalStockProducts: [Product!]!
     }
 
     type ManufacturerStockValue {
         manufacturerName: String!
         totalValue: Float!
+    }
+
+    #  mutation types
+    input ContactInput {
+        name: String!
+        email: String!
+        phone: String!
+    }
+
+    input ManufacturerInput {
+        name: String!
+        country: String
+        website: String
+        description: String
+        address: String
+        contact: ContactInput!
+    }
+
+    # Full input, används vid create
+    input ProductInput {
+        name: String!
+        sku: String!
+        description: String
+        price: Int!
+        category: String
+        manufacturer: ManufacturerInput!
+        amountInStock: Int!
+    }
+
+    # Update-input, alla fält optional
+    input ProductUpdateInput {
+        name: String
+        sku: String
+        description: String
+        price: Int
+        category: String
+        manufacturer: ManufacturerInput
+        amountInStock: Int
+    }
+
+    type Mutation {
+        addProduct(input: ProductInput!): Product
+        updateProduct(id: ID!, input: ProductUpdateInput!): Product
+        deleteProduct(id: ID!): Boolean!
     }
 `;
